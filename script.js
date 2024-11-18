1b825df8d6d0f33fb560b1e46d4cd348cee826ca
ThembiTech <thembisaskosana785@gmail.com>
2024-11-18 22:43:58 +0200

    Updated JS and css

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
 
-    function showDate(event) {
-         if (event.target.value.length > 0) {
-            let currentTime = moment()
-                .tz(event.target.value)
-                .format("dddd, MMMM D, YYYY h:mm A");
+function updateTime(cityId, timezone) {
+    const timeElement = document.getElementById(`${cityId}-time`);
+    const dateElement = document.getElementById(`${cityId}-date`);
+    const now = moment().tz(timezone);
 
-            alert(`It is ${currentTime} in ${event.target.value}`);
-        }
+    timeElement.textContent = now.format("h:mm:ss A");
+    dateElement.textContent = now.format("MMMM Do YYYY");
 
+    
+    setTimeout(() => updateTime(cityId, timezone), 1000);
+}
 
+function addCity(cityName, timezone) {
+    const container = document.querySelector(".container");
+
+    const existingCity = document.querySelector(`#${cityName.replace(/\s+/g, "")}-container`);
+    if (existingCity) {
+        alert(`${cityName} is already displayed.`);
+        return;
     }
-    let selectElement = document.querySelector('#city');
-    selectElement.addEventListener("change",showDate);
+
+    
+    const cityContainer = document.createElement("div");
+    cityContainer.classList.add("city");
+    cityContainer.id = `${cityName.replace(/\s+/g, "")}-container`;
+
+    cityContainer.innerHTML = `
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date" id="${cityName.replace(/\s+/g, "")}-date">Loading date...</div>
+        </div>
+        <div class="time" id="${cityName.replace(/\s+/g, "")}-time">Loading time...</div>
+    `;
+
+    container.insertBefore(cityContainer, container.children[2]);
+
+    updateTime(cityName.replace(/\s+/g, ""), timezone);
+}
+
+
+updateTime("capeTown", "Africa/Johannesburg");
+updateTime("eastLondon", "Africa/Johannesburg");
+
+
+document.getElementById("city").addEventListener("change", function () {
+    const selectedCity = this.value;
+    const cityName = this.options[this.selectedIndex].text;
+
+    if (selectedCity) {
+        addCity(cityName, selectedCity);
+    }
+});
